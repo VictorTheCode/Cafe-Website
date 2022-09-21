a3a944f2b52e05ecba18471bff148b60f332a250
Victor Okpaire <victorokpaire188@gmail.com>
2022-09-21 14:55:50 +0100

    Cafe Model Website

---
+++ index.js
@@ -0,0 +1,26 @@
+window.onscroll = function() {
+    myFunction()
+};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+} 
+
+function openMenu(evt, menuName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("menu");
+    for (i = 0, i < x.length; i++;) {
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace("")
+    }
+}
